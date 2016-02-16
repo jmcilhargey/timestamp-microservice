101cb37b5b1991e7d94fed3cb40a1c86a303fe2e
jmcilhargey <jmcilhargey@gmail.com>
2016-02-16 05:08:44 +0000

    Project upload

--- server.js
+++ server.js
@@ -4,21 +4,51 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 app.use(express.logger("dev"));
 app.use(express.static(__dirname));
 app.use(express.bodyParser());
 
 app.post("/", function(req, res) {
-  let date = req.body.info;
-  res.json(200, date);
+  var date = req.body.info;
+  
+  if (isNaN(date)) {
+    var unixTime = new Date(date).getTime() / 1000;
+    
+    res.json({ unix: unixTime, standard: date })
+  }
+  
+  if (/\d{8}/.test(date)) {
+    var standardTime = new Date(date * 1000);
+    var month = standardTime.getMonth();
+    var day = standardTime.getDate();
+    var year = standardTime.getFullYear();
+    
+    res.json({ unix: date, standard: months[month] + ", " + day + " " + year })
+  }
+  
 });
 
 app.get("/:date", function(req, res) {
-  let date = req.params.date
-  res.json(200, { "hello" : date });
+  var date = req.params.date
+
+  if (isNaN(date)) {
+    var unixTime = new Date(date).getTime() / 1000;
+    
+    res.json({ unix: unixTime, standard: date })
+  }
+  
+  if (/\d{8}/.test(date)) {
+    var standardTime = new Date(date * 1000);
+    var month = standardTime.getMonth();
+    var day = standardTime.getDate();
+    var year = standardTime.getFullYear();
+    
+    res.json({ unix: date, standard: months[month] + ", " + day + " " + year })
+  }
 });
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Ready!");
+  console.log("Ready! Listening on port " + process.env.PORT);
 });
